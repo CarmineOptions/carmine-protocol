a88c0a9c866a0bb0f8dee74584bf5a37a6fdb8f6
Marek Hauzr <marek.hauzr@gmail.com>
2022-05-18 18:53:11 +0200

    empty test for black scholes formula

--- tests/test_option_pricing.py
+++ tests/test_option_pricing.py
@@ -14,7 +14,7 @@ UNIT = 10**16
 
 
 @pytest.mark.asyncio
-async def test_increase_balance() -> None:
+async def test_std_normal_cdf() -> None:
     # Create a new Starknet class that simulates the StarkNet
     # system.
     starknet = await Starknet.empty()
@@ -27,6 +27,42 @@ async def test_increase_balance() -> None:
     for x in range(-50, 50):
         expected = norm.cdf(x / 10)
         # Check the result of std_normal_cdf().
-        result = await contract.std_normal_cdf(x=int(x * (10**16) / 10)).call()
+        result = await contract.std_normal_cdf(x=int(x * UNIT / 10)).call()
         # Maximal relative error on this test set is 0.0027662877116225743.
         assert math.isclose(expected, result.result[0] / result.result[1], rel_tol=0.0028)
+
+
+# @pytest.mark.asyncio
+# async def test_d1_d2() -> None:
+#     # Create a new Starknet class that simulates the StarkNet
+#     # system.
+#     starknet = await Starknet.empty()
+#
+#     # Deploy the contract.
+#     contract = await starknet.deploy(source=CONTRACT_FILE,)
+#
+#     contract.d1_d2(
+#         sigma=0.01,
+#         time_till_maturity_annualized=0.1,
+#         strike_price=100,
+#         underlying_price=100,
+#         risk_free_rate_annualized=0.03
+#     )
+
+
+@pytest.mark.asyncio
+async def test_black_scholes() -> None:
+    # Create a new Starknet class that simulates the StarkNet
+    # system.
+    starknet = await Starknet.empty()
+
+    # Deploy the contract.
+    contract = await starknet.deploy(source=CONTRACT_FILE,)
+
+    contract.black_scholes(
+        sigma=int(0.01 * UNIT),
+        time_till_maturity_annualized=int(0.1 * UNIT),
+        strike_price=int(100 * UNIT),
+        underlying_price=int(100 * UNIT),
+        risk_free_rate_annualized=int(0.03 * UNIT)
+    )
