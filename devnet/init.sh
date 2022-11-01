ab6371341e2ae90494d44dbc138ee34a88bf61c3
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-11-01 20:49:38 +0100

    fix: better var export

--- devnet/init.sh
+++ devnet/init.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+FILE_NAME="_current_vars.env"
+
+# clear/create the vars file
+echo "" >$FILE_NAME
+
+store_var() {
+  # add variable in the form
+  # export KEY=VALUE
+  echo "export $1=${!1}" >>$FILE_NAME
+}
+
 echo "Initialising..."
 
 MAIN_OUTPUT=$(starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http://devnet:5050/ --feeder_gateway_url http://devnet:5050/ deploy --contract /carmine/build/amm.json --no_wallet --salt 0x666)
 
-echo "Deployed main contract"
-
-echo $MAIN_OUTPUT
+echo "Deployed main contract..."
 
 [[ $MAIN_OUTPUT =~ ([a-z0-9]{66}) ]] && export MAIN_CONTRACT_ADDRESS=${BASH_REMATCH[0]}
-
-echo $MAIN_CONTRACT_ADDRESS
 
 LPTOKEN_OUTPUT=$(starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http://devnet:5050/ --feeder_gateway_url http://devnet:5050/ deploy --contract ./build/lptoken.json --no_wallet --salt 0x666 --inputs 111 11 18 0 0 $ACCOUNT_0_ADDRESS $MAIN_CONTRACT_ADDRESS --network alpha-goerli)
 
 [[ $LPTOKEN_OUTPUT =~ ([a-z0-9]{66}) ]] && export LPTOKEN_CONTRACT_ADDRESS=${BASH_REMATCH[0]}
 
-echo "Deployed lptoken"
-
-echo $LPTOKEN_OUTPUT
-
-echo $LPTOKEN_CONTRACT_ADDRESS
-
-echo "Depositing liquidity..."
+echo "Deployed lptoken..."
 
 starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http://devnet:5050/ --feeder_gateway_url http://devnet:5050/ invoke --address $MAIN_CONTRACT_ADDRESS --abi ./build/amm_abi.json --function add_lptoken --inputs $USD_ADDRESS $ETH_ADDRESS $OPTION_TYPE $LPTOKEN_CONTRACT_ADDRESS
 
@@ -30,11 +31,11 @@ starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http:
 
 starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http://devnet:5050/ --feeder_gateway_url http://devnet:5050/ invoke --address $MAIN_CONTRACT_ADDRESS --abi ./build/amm_abi.json --function deposit_liquidity --inputs $ETH_ADDRESS $USD_ADDRESS $ETH_ADDRESS $OPTION_TYPE 0x1bc16d674ec80000 0
 
-echo "Deposited liquidity"
+echo "Deposited liquidity..."
 
 OPTION_1_OUT=$(starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http://devnet:5050/ --feeder_gateway_url http://devnet:5050/ deploy --contract ./build/option_token.json --no_wallet --salt 0x666 --inputs 111 11 18 0 0 $ACCOUNT_0_ADDRESS $MAIN_CONTRACT_ADDRESS $USD_ADDRESS $ETH_ADDRESS $OPTION_TYPE $STRIKE_PRICE $MATURITY_1 $OPTION_SIDE_LONG)
 
-echo "OPTION 1 deployed: $OPTION_1_OUT"
+echo "Deployed first option..."
 
 [[ $OPTION_1_OUT =~ ([a-z0-9]{66}) ]] && export OPTION_TOKEN_ADDRESS_1=${BASH_REMATCH[0]}
 
@@ -42,14 +43,25 @@ starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http:
 
 TEST_BALANCE=$(starknet --chain_id 0x534e5f474f45524c49 --network_id devnet --gateway_url http://devnet:5050/ --feeder_gateway_url http://devnet:5050/ call --address $OPTION_TOKEN_ADDRESS_1 --abi ./build/lptoken_abi.json --function balanceOf --inputs $ACCOUNT_0_ADDRESS)
 
-echo "test balance: $TEST_BALANCE"
-echo ""
-echo "export MAIN_CONTRACT_ADDRESS=${MAIN_CONTRACT_ADDRESS}"
-echo "export LPTOKEN_CONTRACT_ADDRESS=${LPTOKEN_CONTRACT_ADDRESS}"
-echo "export OPTION_TOKEN_ADDRESS_1=${OPTION_TOKEN_ADDRESS_1}"
+echo "Test option balance is $TEST_BALANCE"
 
-echo $(export -p)
+echo "Storing ENV VARS"
 
-echo $(export -p) >>_current_vars.env
+store_var "MAIN_CONTRACT_ADDRESS"
+store_var "LPTOKEN_CONTRACT_ADDRESS"
+store_var "OPTION_TOKEN_ADDRESS_1"
+store_var "ACCOUNT_0_ADDRESS"
+store_var "ACCOUNT_0_PUBLIC"
+store_var "ACCOUNT_0_PRIVATE"
+store_var "ETH_ADDRESS"
+store_var "USD_ADDRESS"
+store_var "STRIKE_PRICE"
+store_var "MATURITY_1"
+store_var "OPTION_TYPE"
+store_var "OPTION_SIDE_LONG"
+store_var "OPTION_SIDE_SHORT"
+store_var "INITIAL_VOLATILITY"
+
+echo "Done! Use ENV VARS from \"${FILE_NAME}\""
 
 exec "$@"
