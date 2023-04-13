6115024eac31dac5905cd97d1ebaff97b896544c
Andy <chepelau.a@gmail.com>
2023-04-13 15:35:03 +0200

    update keeper script

--- scripts/keeper.py
+++ scripts/keeper.py
@@ -1,4 +1,5 @@
 
+import requests
 import asyncio
 import json
 import time
@@ -7,6 +8,8 @@ import os
 import argparse
 import logging
 from typing import List
+from dataclasses import dataclass
+import traceback
 
 from starknet_py.contract import Contract
 from starknet_py.net import AccountClient, KeyPair
@@ -16,12 +19,23 @@ from starknet_py.net.models.chains import StarknetChainId
 from starknet_py.net.client_models import TransactionStatus
 from starknet_py.transaction_exceptions import TransactionRejectedError
 
-MAX_FEE=int(1e16)
+
+MAX_FEE = int(1e16)
+# MAX_FEE = int(1e3)
 SUPPORTED_NETWORKS = ['testnet', 'mainnet']
 
-def parse_envs() -> List[str]:
+@dataclass
+class EnVars:
+    private_key: str
+    public_key: str
+    tg_key: str
+    tg_chat_id: str
+
+def parse_envs() -> EnVars:
     PRIVATE_KEY = os.getenv('PRIVATE_KEY')
     PUBLIC_KEY = os.getenv('PUBLIC_KEY')
+    TG_KEY = os.getenv("TG_KEY")
+    TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 
     if PRIVATE_KEY == None:
         raise ValueError("Missing PRIVATE_KEY ENV")
@@ -29,13 +43,24 @@ def parse_envs() -> List[str]:
     if PUBLIC_KEY == None:
         raise ValueError("Missing PUBLIC_KEY ENV")
 
-    return [PRIVATE_KEY,PUBLIC_KEY]
+    if TG_KEY == None:
+        raise ValueError("Missing TG_KEY ENV")
+
+    if TG_CHAT_ID == None:
+        raise ValueError("Missing TG_CHAT_ID ENV")
+
+    return EnVars(
+        private_key = PRIVATE_KEY,
+        public_key = PUBLIC_KEY,
+        tg_key = TG_KEY,
+        tg_chat_id = TG_CHAT_ID
+    )
 
 def setup_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-                        prog = 'Starknet Keeper bot',
-                        description = 'Periodically calls predefined function on Starknet',
-            )
+        prog='Starknet Keeper bot',
+        description='Periodically calls predefined function on Starknet',
+    )
 
     parser.add_argument(
         '--net', '-n', type = str
@@ -53,7 +78,7 @@ def setup_parser() -> argparse.ArgumentParser:
         '--function_name', '-f', type = str
     )
     parser.add_argument(
-        '--function_arguments', '-fa', action='append', default = []
+        '--function_arguments', default = [], type = json.loads
     )
     return parser
 
@@ -63,71 +88,98 @@ def get_abi(args: argparse.Namespace) -> List:
 
     return abi
 
+def alert(msg: str, chat_id: str, api_key: str):
+    # https://api.telegram.org/bot[BOT_API_KEY]/sendMessage?chat_id=[MY_CHANNEL_NAME]&text=[MY_MESSAGE_TEXT]
+
+    params = {
+        'chat_id': chat_id,
+        'text': msg,
+    }
+    res = requests.get("https://api.telegram.org/bot" + api_key + "/sendMessage", params=params)
+    res.raise_for_status()
+
+
 def get_chain(args: argparse.Namespace) -> StarknetChainId:
     if args.net not in SUPPORTED_NETWORKS:
-        raise ValueError(f'Unknown network, expected one of {SUPPORTED_NETWORKS}, got: {args.net}')
+        raise ValueError(
+            f'Unknown network, expected one of {SUPPORTED_NETWORKS}, got: {args.net}'
+        )
 
     return StarknetChainId.TESTNET if args.net == 'testnet' else StarknetChainId.MAINNET
 
 async def main():
 
     logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s', 
-        datefmt='%d-%b-%y %H:%M:%S', 
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%d-%b-%y %H:%M:%S',
         level=logging.INFO
     )
-
-    parser = setup_parser()
-    args = parser.parse_args()
-    private_key, public_key = parse_envs()
-
-    logging.info(f"Parsed args: {args}")
-
-    abi = get_abi(args.abi_path)   
-    chain = get_chain(args)
-
-    logging.info(f"Selected chain: {chain}")
-
-    client = GatewayClient(net=args.net)
-    account = Account(
-        client = client,
-        address = args.wallet_address,
-        key_pair = KeyPair(
-            private_key = int(private_key, 16),
-            public_key = int(public_key, 16)
-        ),
-        chain=chain,
-    )
-    
-    contract = Contract(
-        address = args.contract_address,
-        abi = abi,
-        provider = account,
-    )
-    
-    # Invoke function
-    call = contract.functions[args.function_name].prepare(
-        *args.function_arguments,
-    )
-
-    logging.info(f"Executing call: {call}")
-
-    response = await account.execute(calls=call, max_fee= MAX_FEE)
-
-    logging.info(f"Sent transaction: {response}")
-
-    await account.client.wait_for_tx(response.transaction_hash)
-
+    enVars = parse_envs()
     try: 
+        parser = setup_parser()
+        args = parser.parse_args()
+
+        logging.info(f"Parsed args: {args}")
+
+        abi = get_abi(args)   
+        chain = get_chain(args)
+
+        logging.info(f"Selected chain: {chain}")
+
+        client = GatewayClient(net=args.net)
+        account = Account(
+            client = client,
+            address = args.wallet_address,
+            key_pair = KeyPair(
+                private_key = int(enVars.private_key, 16),
+                public_key = int(enVars.public_key, 16)
+            ),
+            chain=chain,
+        )
+        
+        contract = Contract(
+            address = args.contract_address,
+            abi = abi,
+            provider = account,
+        )
+        
+        # Invoke function
+        call = contract.functions[args.function_name].prepare(
+            *args.function_arguments,
+        )
+
+        logging.info(f"Executing call: {call}")
+
+        tracebacks = []
+
+        for _ in range(3):
+            response = await account.execute(calls = call, max_fee = MAX_FEE)
+            logging.info(f"Sent transaction: {response}")
+            try: 
+                await account.client.wait_for_tx(response.transaction_hash)
+                break
+            except Exception as err:
+                logging.error(f"Transaction Rejected: {err}")
+                tracebacks.append("".join(traceback.format_exception(err, value=err, tb=err.__traceback__)))
+                continue
+
         tx_status = await account.client.get_transaction_receipt(response.transaction_hash)
 
-        if (tx_status.status == TransactionStatus.ACCEPTED_ON_L1 ) or (tx_status.status == TransactionStatus.ACCEPTED_ON_L2):
-            logging.info(f"Tx receipt: {tx_status}")
+        if (tx_status.status == TransactionStatus.ACCEPTED_ON_L1) or (tx_status.status == TransactionStatus.ACCEPTED_ON_L2):
+            logging.info(f"Tx SUCCESSFULL, receipt: {tx_status}")
         else:
-            logging.error(f"Tx not accepted: {tx_status}")
+            tracebacks.append(tx_status)
+            logging.error(f"Tx NOT ACCEPTED: {tx_status}")
 
-    except TransactionRejectedError as err:
-        logging.error(f"Transaction Rejected: {err}")
+        
+        if tracebacks:
+            alert(f"Received {len(tracebacks)} errors:(( - \n {tracebacks}", enVars.tg_chat_id, enVars.tg_key)
+        else: 
+            alert(f"Update successfull: {tx_status} \n {call}", enVars.tg_chat_id, enVars.tg_key)
+
+    except Exception as e:
+        err_msg = "".join(traceback.format_exception(err, value=err, tb=err.__traceback__))
+        alert(f"COMPLETE FAIL: {err_msg}", enVars.tg_chat_id, enVars.tg_key)
 
 if __name__ == '__main__':
     asyncio.run(main())
@@ -135,12 +187,14 @@ if __name__ == '__main__':
 # Example usage
 #   PRIVATE_KEY=... \
 #   PUBLIC_KEY=... \
+#   TG_CHAT_ID=...\
+#   TG_KEY=...\
 #   python ./keeper.py \
 #       --net testnet \
 #       -wa $ACCOUNT_ADDR \
 #       --contract_address $DUMMY_ADDR \
 #       --abi_path ../build/dummy_abi.json \
 #       --function_name write_value \
-#       -fa 10 -fa 2
+#       --function_arguments "[1, 2]"
 
 
