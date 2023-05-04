c82921f7e62668ebd46db65dd6e1fab759766f8b
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2023-05-04 11:13:55 +0000

    Polish lpool overview with pool balances and async

--- scripts/lpool_overview.py
+++ scripts/lpool_overview.py
@@ -23,6 +23,43 @@ class Option:
     def get_strike_price(self):
         return self.strike_price // 2**61
 
+async def get_pool_unlocked_capital(lpt_addr, decimals: int, currency: str):
+    get_unlocked_capital_call = Call(
+        to_addr=AMM_ADDR,
+        selector=get_selector_from_name('get_unlocked_capital'),
+        calldata=[lpt_addr]
+    )
+    res = await client.call_contract(get_unlocked_capital_call)
+    capital_human = res[0] / 10**decimals
+    return f'Unlocked capital {capital_human} {currency}'.format()
+
+
+async def get_pool_balance(lpt_addr, decimals: int, currency: str):
+    get_balance_call = Call(
+        to_addr=AMM_ADDR,
+        selector=get_selector_from_name('get_lpool_balance'),
+        calldata=[lpt_addr]
+    )
+    res = await client.call_contract(get_balance_call)
+    capital_human = res[0] / 10**decimals
+    return f'Total capital {capital_human} {currency}'.format()
+
+
+async def get_pool_position_in_option(option, relevant_pool_addr):
+    get_option_position_call = Call(
+        to_addr=AMM_ADDR,
+        selector=get_selector_from_name('get_option_position'),
+        calldata=[relevant_pool_addr, option.option_side, option.maturity, option.strike_price],
+    )
+    res = await client.call_contract(get_option_position_call)
+    formatted_date = datetime.utcfromtimestamp(option.maturity).strftime('%m%d')
+    side = 'long' if option.option_side == 0 else 'short'
+    type = 'call' if option.option_type == 0 else 'put'
+    position_human = res[0] / 10**18
+    return f'Pool option pos in {side} {type} {option.get_strike_price()} {formatted_date} is {position_human}'.format()
+    
+
+
 async def main():
     all_lptokens_call = Call(
         to_addr=AMM_ADDR,
@@ -57,32 +94,23 @@ async def main():
         options_put.append(Option(res[i], res[i+1], res[i+2], res[i+3], res[i+4], res[i+5]))
         i += 6
 
+    # pool unlocked capital
+    # pool total capital
+    coros = [
+        get_pool_unlocked_capital(call_lptoken_addr, 18, 'ETH'),
+        get_pool_unlocked_capital(put_lptoken_addr, 6, 'USDC'),
+        get_pool_balance(call_lptoken_addr, 18, 'ETH'),
+        get_pool_balance(put_lptoken_addr, 6, 'USDC')
+    ]
 
+    # pool position in options
     for option in options_call:
-        get_option_position_call = Call(
-            to_addr=AMM_ADDR,
-            selector=get_selector_from_name('get_option_position'),
-            calldata=[call_lptoken_addr, option.option_side, option.maturity, option.strike_price],
-        )
-        res = await client.call_contract(get_option_position_call)
-        formatted_date = datetime.utcfromtimestamp(option.maturity).strftime('%d%m')
-        position_human = res[0] / 10**18
-        side = 'long' if option.option_side == 0 else 'short'
-        if position_human > 0.01:
-            print(f'Pool option position in {side} call {option.get_strike_price()} {formatted_date} is {position_human}'.format())
-    
+        coros.append(get_pool_position_in_option(option, call_lptoken_addr))
     for option in options_put:
-        get_option_position_call = Call(
-            to_addr=AMM_ADDR,
-            selector=get_selector_from_name('get_option_position'),
-            calldata=[put_lptoken_addr, option.option_side, option.maturity, option.strike_price],
-        )
-        res = await client.call_contract(get_option_position_call)
-        formatted_date = datetime.utcfromtimestamp(option.maturity).strftime('%m%d')
-        position_human = res[0] / 10**18
-        side = 'long' if option.option_side == 0 else 'short'
-        if position_human > 0.01:
-            print(f'Pool option position in {side} put {option.get_strike_price()} {formatted_date} is {position_human}'.format())
+        coros.append(get_pool_position_in_option(option, put_lptoken_addr))
+    results = await asyncio.gather(*coros)
+    for result in results:
+        print(result)
 
 
 if __name__ == "__main__":
