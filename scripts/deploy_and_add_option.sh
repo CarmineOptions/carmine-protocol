b8259381b4abcd48bbffe86b5f1f608f95a2504c
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2023-09-14 09:20:08 +0000

    Simplify deploy of options – testnet

--- scripts/deploy_and_add_option.sh
+++ scripts/deploy_and_add_option.sh
@@ -7,7 +7,7 @@ export STARKNET_WALLET=starkware.starknet.wallets.open_zeppelin.OpenZeppelinAcco
 FEEDER_GATEWAY_URL=https://alpha4.starknet.io/feeder_gateway/
 #GATEWAY_URL=https://alpha4-2.starknet.io/gateway/
 GATEWAY_URL=https://alpha4.starknet.io/gateway/
-cp ~/.starknet_accounts/starknet_open_zeppelin_accounts-testnet1.json ~/.starknet_accounts/starknet_open_zeppelin_accounts.json
+#cp ~/.starknet_accounts/starknet_open_zeppelin_accounts-testnet1.json ~/.starknet_accounts/starknet_open_zeppelin_accounts.json
 
 #ADMIN=0x003f47b0187bcdde504e83f39a31900207712e0383ee1ac3687eea5af4a02252 # testnet 1
 #ADMIN=0x54beabcdd71b0735edc2cc67e86da69a30c0fed3a91a33bae88244744bba1ed # testnet 2
@@ -21,24 +21,22 @@ LPTOKEN_ADDRESS_CALL=16704915925421536395149338322678881764854029690973745514150
 LPTOKEN_ADDRESS_PUT=86564211892987183020075958831889857811281626806290146628813165299578849650 # testnet1 eth/usd put pool
 #LPTOKEN_ADDRESS=367852766564488059499954238561392360215903199117051217276115258544759204227 # testnet2 call pool
 #LPTOKEN_ADDRESS=2738355697057021849185852497074326827930099024415976588426758511175104190904 # testnet2 put pool
-INITIAL_VOLATILITY=103762935414616227840
+INITIAL_VOLATILITY=86238528544592150528
 
 # ADJUST THESE BEFORE RUNNING
 
-OPTION_TYPE=1
+OPTION_TYPE=0
 
 #STRIKE_PRICE=4381101717506018508800  # 1900
-#STRIKE_PRICE=4150517416584649113600  # 1800
+STRIKE_PRICE=4150517416584649113600  # 1800
 #STRIKE_PRICE=3919933115663279718400  # 1700
 #STRIKE_PRICE=3689348814741910323200  # 1600
-STRIKE_PRICE=3458764513820540928000  # 1500
+#STRIKE_PRICE=3458764513820540928000  # 1500
 #STRIKE_PRICE=3228180212899171532800  # 1400
 #STRIKE_PRICE=2997595911977802137600 # 1300
 #STRIKE_PRICE=2767011611056432742400 # 1200
-#MATURITY=1671148799 #15th dec
-#MATURITY=1672358399 # 29th dec
-MATURITY=1689897599  # Thu Jul 20 2023 23:59:59 GMT+0000
-OPTION_SIDE=1
+MATURITY=1698451199 # Fri Oct 27 2023 23:59:59 GMT+0000
+OPTION_SIDE=0
 CLASS_HASH=0x0489c4d9adf068ae5198f9bd180450fdf5aceb5e6989a958b8833cb45f1f2b6c # testnet1
 #CLASS_HASH=0x0303cbc2300f9b00d77098431703eeae61b4985247856ff13e6d2d0bebbc612a # testnet2, class hash for option token
 
@@ -52,28 +50,27 @@ starknet deploy --gateway_url $GATEWAY_URL --feeder_gateway_url $FEEDER_GATEWAY_
 
 echo "Please paste the contract address and press Enter"
 read OPTION_TOKEN_ADDRESS_HEX
-echo "Please paste the tx hash and press Enter"
-read TXHASH
 
 OPTION_TOKEN_ADDRESS=$(echo $OPTION_TOKEN_ADDRESS_HEX | python scripts/todec.py)
-SUCCESS=n
-sleep 10
-while [ y$SUCCESS != "yy" ]
-do
-    starknet get_transaction_receipt --feeder_gateway_url $FEEDER_GATEWAY_URL --hash $TXHASH
-    echo "Does it look like the transaction was successful? y to continue, Ctrl-C to abort, anything else to keep waiting"
-    read SUCCESS
-done
+sleep 5
+# SUCCESS=n
+# sleep 2
+# while [ y$SUCCESS != "yy" ]
+# do
+#     starknet get_transaction_receipt --feeder_gateway_url $FEEDER_GATEWAY_URL --hash $TXHASH
+#     echo "Does it look like the transaction was successful? y to continue, Ctrl-C to abort, anything else to keep waiting"
+#     read SUCCESS
+# done
 
 
 starknet invoke --gateway_url $GATEWAY_URL --feeder_gateway_url $FEEDER_GATEWAY_URL --address $AMM_ADDRESS --abi abi/v0.1.10/amm_abi.json --function add_option --inputs $OPTION_SIDE $MATURITY $STRIKE_PRICE $QUOTE_TOKEN_ADDRESS $BASE_TOKEN_ADDRESS $OPTION_TYPE $LPTOKEN_ADDRESS $OPTION_TOKEN_ADDRESS $INITIAL_VOLATILITY --max_fee 2560986752518800
-echo "Please paste the tx hash from above here"
-read TXHASH
-SUCCESS=n
-sleep 10
-while [ y$SUCCESS != "yy" ]
-do
-    starknet get_transaction_receipt --feeder_gateway_url $FEEDER_GATEWAY_URL --hash $TXHASH
-    echo "Does it look like the transaction was successful? y to continue, Ctrl-C to abort, anything else to keep waiting"
-    read SUCCESS
-done
+# echo "Please paste the tx hash from above here"
+# read TXHASH
+# SUCCESS=n
+# sleep 10
+# while [ y$SUCCESS != "yy" ]
+# do
+#     starknet get_transaction_receipt --feeder_gateway_url $FEEDER_GATEWAY_URL --hash $TXHASH
+#     echo "Does it look like the transaction was successful? y to continue, Ctrl-C to abort, anything else to keep waiting"
+#     read SUCCESS
+# done
